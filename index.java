85c59d631707238d333d1d147432bd1f2d26a3bd
butdory <67633798+butdory@users.noreply.github.com>
2021-10-28 14:06:24 +0900

    Add files via upload

--- index.java
+++ index.java
@@ -8,18 +8,24 @@ import javax.swing.JFrame;
 
 public class index extends JFrame{
 	
-	public index() {
+	public index(String name, int age) {
 		setTitle("메인 프레임");
 		setSize(1280 , 720);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
+		String char_age = Integer.toString(age);
+		JLabel my_label_1 = new JLabel(name);
+		JLabel my_label_2 = new JLabel(char_age);
 		
-		JButton my_btn_1 = new JButton("확인");
-		JButton my_btn_2 = new JButton("취소");
+		String header[] = {"이름", "나이"};
+		String contents[][] = {{name, char_age}};
 		
-		c.add(my_btn_1);
-		c.add(my_btn_2);
+		JTable table = new JTable(contents, header);
+		
+		c.add(my_label_1);
+		c.add(my_label_2);
+		c.add(table);
 		
 		setVisible(true);
 	}
@@ -30,9 +36,7 @@ public class index extends JFrame{
 		String[] name = {"정원영", "이창학", "윤도현", "나제원"};
 		
 		interfacetest getstudent = new interfacetest();
-		System.out.println(getstudent.getName(name, 3));
-		System.out.println(getstudent.getAge(age, 3));
-		new index();
+		new index(getstudent.getName(name, 3), getstudent.getAge(age, 3));
 	}
 
 }
